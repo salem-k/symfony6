96a44c654e14ca8af502d1a670408d8668b96b55
Mac <kammoun.salem@gmail.com>
2022-05-07 18:28:02 +0100

    2

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -20,6 +20,19 @@ class ApiController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
+        var_dump($_POST);
+        
+        /**
+        $conn = pg_connect("host=localhost");
+
+        SELECT id, username, firstname, lastname, email, created_on, modify_on FROM public.accounts where email = 'kammoun.salem@gmail.com';
+
+        $result = pg_query($conn, "SELECT datname FROM pg_database");
+        while ($row = pg_fetch_row($result)) {
+            echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
+        }
+         */
+ 
         return $this->json([
             'message' => '1111Welcome to your new controller!',
             'path' => 'src/Controller/ApiController.php',
