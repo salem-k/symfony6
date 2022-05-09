c46644a5f11791227aaeb7152136743ab83a53c4
Mac <kammoun.salem@gmail.com>
2022-05-09 20:28:09 +0100

    no message

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ApiController extends AbstractController
 {
@@ -20,12 +22,12 @@ class ApiController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
-        
+        $request = Request::createFromGlobals();
         
         $conn = pg_connect("host=localhost port=5431 dbname=mac");
 
         
-        $selectSqlCommand = "SELECT * FROM public.accounts where email = '".$_REQUEST["email"]."' AND pass = '".$_REQUEST["password"]."'";
+        $selectSqlCommand = "SELECT * FROM public.accounts where email = '".$request->query->get('email')."' AND pass = '".$request->query->get('password')."'";
         
         
         $result = pg_query($conn,$selectSqlCommand);
