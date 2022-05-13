7c5dc2e48c9cc887044421632e5b650a40a90b77
Mac <kammoun.salem@gmail.com>
2022-05-13 10:36:31 +0100

    no message

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -21,6 +21,35 @@ class ApiController extends AbstractController
         ]);
     }
 
+    #[Route('/uploadbackground', name: 'uploadbackground_api')]
+    public function uploadbackground(): Response
+    {
+
+        $request = Request::createFromGlobals();
+
+        $myRequest = json_decode($request->getContent());
+
+
+        $backgroundFileName = md5(uniqid());
+        foreach($request->files as $uploadedFile) {
+            $uploadedFile->move( $this->getParameter('uploads_dir_background'), $backgroundFileName );
+        }
+        
+
+        $conn = pg_connect("host=localhost port=5431 dbname=mac");
+        $selectSqlCommand = "SELECT id_video, title, duration, path, created_on, modify_on FROM public.video ORDER BY id_video DESC limit 1";
+        $result = pg_query($conn,$selectSqlCommand);
+        $row = pg_fetch_row($result);
+
+        //$insertSqlCommand = "INSERT INTO public.video(id_video, title, duration, path, created_on, modify_on) VALUES ($row[0]+1, '".$request->request->all()["nomduprojet"]."', 100, '".$videoFilename."', current_timestamp, current_timestamp);";
+        //$result = pg_query($conn,$insertSqlCommand);
+        
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiController.php',
+        ]);
+    }
+    
     #[Route('/videoadd', name: 'videoadd_api')]
     public function videoadd(): Response
     {
@@ -41,7 +70,7 @@ class ApiController extends AbstractController
         $result = pg_query($conn,$selectSqlCommand);
         $row = pg_fetch_row($result);
 
-        $insertSqlCommand = "INSERT INTO public.video(id_video, title, duration, path, created_on, modify_on) VALUES ($row[0]+1, '".$request->request->all()["npmduprojet"]."', 100, '".$videoFilename."', current_timestamp, current_timestamp);";
+        $insertSqlCommand = "INSERT INTO public.video(id_video, title, duration, path, created_on, modify_on) VALUES ($row[0]+1, '".$request->request->all()["nomduprojet"]."', 100, '".$videoFilename."', current_timestamp, current_timestamp);";
 
 
 
